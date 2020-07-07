d688fe62a82e8ef4e1f0d5d50c2c91c356b25a17
Todd G <todd@dapix.io>
2020-07-06 21:43:24 -0600

    spaces provide no benefit, remove

--- logging/logging.go
+++ logging/logging.go
@@ -3,7 +3,6 @@ package logging
 import (
 	"log"
 	"os"
-	"strings"
 )
 
 func Setup(prefix string) (err *log.Logger, info *log.Logger, debug *log.Logger) {
@@ -12,9 +11,6 @@ func Setup(prefix string) (err *log.Logger, info *log.Logger, debug *log.Logger)
 		i *log.Logger
 		d *log.Logger
 	)
-	if len(prefix) < 24 {
-		prefix = prefix + strings.Repeat(" ", 24-len(prefix))
-	}
 	e = log.New(os.Stderr, prefix+" ERROR: ", log.Lshortfile|log.LstdFlags|log.Lmsgprefix)
 	i = log.New(os.Stdout, prefix+"  INFO: ", log.Lshortfile|log.LstdFlags|log.Lmsgprefix)
 	d = log.New(os.Stdout, prefix+" DEBUG: ", log.Lshortfile|log.LstdFlags|log.Lmsgprefix)
