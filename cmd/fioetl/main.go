2565f8808d7c0accadf43e3d0aab6fba3a6b8fff
Todd G <todd@dapix.io>
2020-08-19 17:54:28 -0600

    accidentally excluded cmd directory

---
+++ cmd/fioetl/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/dapixio/fio.etl/chronicle"
+	"github.com/dapixio/fio.etl/logging"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func main() {
+	elog, ilog, _ := logging.Setup(" [fioetl-consumer] ")
+	ilog.Println("fioetl starting")
+
+	c := chronicle.NewConsumer("")
+	router := mux.NewRouter()
+	router.HandleFunc("/chronicle", c.Handler)
+	elog.Fatal(http.ListenAndServe(":8844", router))
+}
